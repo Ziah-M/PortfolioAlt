9f177cb0f05f783b5bdd0f73896cadd18ad82268
Ziah-M <65767717+Ziah-M@users.noreply.github.com>
2020-09-11 23:36:22 +1000

    Add Contact Form

--- Pages/Contact/ContactForm.js
+++ Pages/Contact/ContactForm.js
@@ -1,11 +1,57 @@
 import React from 'react'
+import {Form as UnstyledForm, Button as UnstyledButton} from 'react-bootstrap'
+import styled from 'styled-components'
 
 const ContactForm = () => {
     return (
-        <div>
-            Contact Form placeholder
-        </div>
+        <Form>
+            <Label>NAME:</Label>
+            <Control type='text' placeholder='Enter your name here.'/>
+
+            <Label>EMAIL:</Label>
+            <Control type='email' placeholder='Enter your email here.'/>
+
+            <Label>MESSAGE:</Label>
+            <Control type='text' placeholder='Enter your message here.'/>
+
+            <Button variant='primary' type='submit' block>
+                Send Inquiry
+            </Button>
+        </Form>
     )
 }
+
+const Form=styled(UnstyledForm)`
+    width:400px;
+`
+
+const Label=styled(Form.Label)`
+margin:0;
+padding:0;
+font-weight:600;
+font-size:12px;
+user-select:none;
+`
+
+const Control=styled(Form.Control)`
+margin-bottom:20px;
+background:#d2d2d2;
+font-size:12px;
+padding:20px;
+`
+
+const Button=styled(UnstyledButton)`
+    text-transform:uppercase;
+    font-size:12px;
+    padding:10px;
+    background:#e91e63;
+    color:white;
+    border:none;
+    transition:0.3s ease-in-out;
+    &:hover{
+        background:white;
+        color:black;
+    }
+`
 
 export default ContactForm
